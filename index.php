cb1594c2a513dbf249216e493dc43c65fe90c042
Prakash <itprakash2022@gmail.com>
2025-07-31 20:35:27 +0530

    index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ include('include/header.php');
 include('include/navbar.php');
  ?>
 
-<div class="py-5">
+<div class= "py-5">
     <div class="container">
         <div class="row">
             <div class="col-md-12 text-center">
